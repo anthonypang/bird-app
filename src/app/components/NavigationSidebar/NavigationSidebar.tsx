e6d28df077fd1ad1362fc027d873b5d908373480
Anthony Pang <anthonypang99@gmail.com>
2025-01-08 18:15:01 -0500

    initial commit

---
+++ src/app/components/NavigationSidebar/NavigationSidebar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavigationSidebar = () => {
+  return <div className="h-screen bg-green-500">NavigationSidebar</div>;
+};
+
+export default NavigationSidebar;
---
+++ src/app/test/[id]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const page = async ({ params }: { params: { id: string } }) => {
+  const { id } = await params;
+  return <div>{id}</div>;
+};
+
+export default page;
