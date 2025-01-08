5a8a57e87c4ec912e3eb24d0da0c06a346696d3d
Anthony Pang <anthonypang99@gmail.com>
2025-01-08 18:58:01 -0500

    navigation sidebar initial structure

--- src/app/components/NavigationSidebar/NavigationSidebar.tsx
+++ src/app/components/NavigationSidebar/NavigationSidebar.tsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
 import React from "react";
 
+const navItems = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Explore",
+    href: "/explore",
+  },
+];
+
 const NavigationSidebar = () => {
-  return <div className="h-screen bg-green-500">NavigationSidebar</div>;
+  return (
+    <div className="h-screen  flex flex-col">
+      {navItems.map((item, key) => (
+        <Link key={key} href={item.href}>
+          {item.name}
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default NavigationSidebar;
