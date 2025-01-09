641f48b20230aef7953f499cec792da88797fbd6
Anthony Pang <anthonypang99@gmail.com>
2025-01-09 17:20:24 -0500

    replace sidebar divs with reuseable components

--- src/app/components/ExploreSidebar/ExploreSidebar.tsx
+++ src/app/components/ExploreSidebar/ExploreSidebar.tsx
@@ -1,26 +1,17 @@
 import React from "react";
+import TopicSuggestions from "./components/TopicSuggestions/TopicSuggestions";
+import FriendSuggestions from "./components/FriendSuggestions/FriendSuggestions";
+import Search from "../Search/Search";
 
 const ExploreSidebar = () => {
   return (
-    <div className="hidden lg:block px-4">
+    <div className="hidden xl:block px-4 max-w-sm w-full">
       {/* Search Box */}
-      <div className="mt-6">
-        <input
-          type="text"
-          placeholder="Search"
-          className="w-full px-4 py-2 text-gray-700 bg-gray-200 rounded"
-        />
-      </div>
+      <Search />
       {/* Topic Suggestions */}
-      <div className="mt-6">
-        <h3 className="text-sm font-bold text-gray-600">
-          What&apos;s happening
-        </h3>
-      </div>
+      <TopicSuggestions />
       {/* Friend Suggestions */}
-      <div className="mt-6">
-        <h3 className="text-sm font-bold text-gray-600">Who to follow</h3>
-      </div>
+      <FriendSuggestions />
     </div>
   );
 };
