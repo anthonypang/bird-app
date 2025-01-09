05bc6e2ec307df5ef28b27dcd6c049db0c7ae513
Anthony Pang <anthonypang99@gmail.com>
2025-01-09 17:21:00 -0500

    create search component

---
+++ src/app/components/Search/Search.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div className="mt-2">
+      <input type="text" placeholder="Search" className="w-full search" />
+    </div>
+  );
+};
+
+export default Search;
