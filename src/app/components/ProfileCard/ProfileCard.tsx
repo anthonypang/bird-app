7abee903577284ebddb1baf63f7b3a0f5878e229
Anthony Pang <anthonypang99@gmail.com>
2025-01-13 16:22:15 -0500

    create Profile card component

--- src/app/components/ProfileCard/ProfileCard.tsx
+++ src/app/components/ProfileCard/ProfileCard.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Avatar from "../Avatar/Avatar";
 
 export type User = {
   id: number;
@@ -12,31 +12,32 @@ export type User = {
 
 type ProfileCardProps = {
   user: User;
+  isCurrentUser?: boolean;
 };
 
-const ProfileCard = ({ user }: ProfileCardProps) => {
+const ProfileCard = ({ user, isCurrentUser }: ProfileCardProps) => {
   return (
     <Link
       href={""}
       key={user.id}
-      className="flex items-center justify-between hover:bg-gray-900 p-3"
+      className={`flex items-center justify-between hover:bg-gray-900 ${
+        isCurrentUser && "hover:navHover"
+      } p-3`}
     >
-      <div className="flex gap-1">
-        <Image
-          src={user.avatar || "https://i.pravatar.cc/300"}
-          width={40}
-          height={40}
-          alt={user.name}
-          className="w-10 h-10 rounded-full"
-        />
-        <div>
+      <div className="flex gap-2">
+        <Avatar user={user} />
+        <div className={isCurrentUser ? "hidden lg:block" : "block"}>
           <div className="text-sm text-white font-bold ">{user.name}</div>
           <div className="text-sm text-gray-400 ">{user.userName}</div>
         </div>
       </div>
-      <button className="font-bold bg-white text-black rounded-3xl block w-full lg:w-20 py-1.5 text-sm">
-        Follow
-      </button>
+      {!isCurrentUser ? (
+        <button className="font-bold bg-white text-black rounded-3xl block w-full lg:w-20 py-1.5 text-sm">
+          Follow
+        </button>
+      ) : (
+        <button className="hidden lg:block">...</button>
+      )}
     </Link>
   );
 };
