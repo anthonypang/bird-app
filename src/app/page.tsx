46a3674fe8ee3540326cecb8c646ca52df32b494
Anthony Pang <anthonypang99@gmail.com>
2025-01-16 17:27:44 -0500

    render tabs component on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import CreatePost from "./components/CreatePost/CreatePost";
 import PostFeed from "./components/PostFeed/PostFeed";
+import Tabs from "./components/Tabs/Tabs";
 
 export default function Home() {
+  const tabs = [
+    { label: "For You", value: "for-you" },
+    { label: "Following", value: "following" },
+  ];
+
   return (
     <div className="w-full">
-      {/* Tabs Component */}
-      <div className="flex">
-        <button className="tab">For You</button>
-        <button className="tab">Following</button>
-      </div>
-      {/* Create Post Component */}
+      <Tabs tabs={tabs} defaultTab="for-you" />
       <CreatePost />
-      {/* Feed Component */}
       <PostFeed />
     </div>
   );
