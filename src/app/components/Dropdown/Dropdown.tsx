07c58dcd53d1d8e04cbb5ff83de6332b8713d168
Anthony Pang <anthonypang99@gmail.com>
2025-01-17 11:32:52 -0500

    On dropdown outside click, close dropdown

--- src/app/components/Dropdown/Dropdown.tsx
+++ src/app/components/Dropdown/Dropdown.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 type DropdownProps = {
   anchor: React.ReactNode;
@@ -9,13 +9,33 @@ type DropdownProps = {
 
 const Dropdown = ({ anchor, children, className }: DropdownProps) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
   return (
-    <div className={`relative inline-block text-left ${className}`}>
+    <div
+      className={`relative inline-block text-left ${className}`}
+      ref={dropdownRef}
+    >
       <button onClick={toggleDropdown}>{anchor}</button>
       {isDropdownOpen && (
         <div className="origin-top-right absolute mt-2 w-56 rounded-md bg-black z-50 shadow-lg border border-gray-700 p-3">
