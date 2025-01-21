91f79bac1e9589f5357d2e56419af33ad8767210
Anthony Pang <anthonypang99@gmail.com>
2025-01-20 23:12:16 -0500

    update Post component to included and expanded view

--- src/app/components/Post/Post.tsx
+++ src/app/components/Post/Post.tsx
@@ -1,39 +1,53 @@
 import React from "react";
 import Avatar from "../Avatar/Avatar";
-
-export type Post = {
-  id: number;
-  content: string;
-  user: {
-    id: number;
-    name: string;
-    userName: string;
-    avatar: string;
-  };
-};
+import Link from "next/link";
+import { Post as PostType } from "@/app/lib/posts";
 
 type PostProps = {
-  post: Post;
+  post: PostType;
+  isExpanded?: boolean;
 };
 
-const Post = ({ post }: PostProps) => {
-  return (
-    <div className="post">
+const Post = ({ post, isExpanded }: PostProps) => {
+  const content = (
+    <>
       <Avatar user={post.user} />
-      <div className="w-full ml-3 mr-3">
-        <div className="flex gap-1">
-          <span className="font-bold">{post.user.name}</span>
-          <span className="text-gray-500">{post.user.userName}</span>
+      <div className={`w-full ${isExpanded ? "" : "ml-3 mr-3"}`}>
+        <div className={`${isExpanded ? "block" : "flex gap-1"} `}>
+          <div className="font-bold">{post.user.name}</div>
+          <div className="text-gray-500">@{post.user.userName}</div>
         </div>
-        <div>{post.content}</div>
+        <div className={`${isExpanded ? "py-3" : ""}`}>{post.content}</div>
+        {isExpanded && (
+          <div className="text-gray-500 text-sm">3:46 PM · Jan 20, 2025</div>
+        )}
         {/* reactions */}
-        <div className="flex justify-between mt-3">
+        <div
+          className={`flex justify-between mt-3 ${
+            isExpanded ? "border-t border-b border-outlineColor p-2" : ""
+          }`}
+        >
           <button>💬</button>
           <button>🔁</button>
           <button>♥️</button>
           <button>🔖</button>
         </div>
       </div>
+    </>
+  );
+
+  return (
+    <div className="postContainer">
+      {isExpanded ? (
+        <div className="post block hover:bg-transparent cursor-auto">
+          {content}
+          <div>Post your reply</div>
+        </div>
+      ) : (
+        <Link className="post" href={`/${post.user.userName}/${post.id}`}>
+          {content}
+        </Link>
+      )}
     </div>
   );
 };
